049de27a180e5ee41212ecc2e55941863bd13ffe
yourdocuments <sakhijahanusha@gmail.com>
2026-09-21 02:50:08 +0600

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // Supabase এর দুইটা তথ্য এখানে বসান (Project Settings > API থেকে)
-const SUPABASE_URL = "YOUR_SUPABASE_URL";
-const SUPABASE_ANON_KEY = "YOUR_SUPABASE_ANON_KEY";
+const SUPABASE_URL = "https://qfahxdpbwcqelmzqpxli.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_DokglzkOHskukAI71QJ9wQ_d4esduLN";
